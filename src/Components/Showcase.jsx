3265c76be98bf21d413e4035c2375ab71d5fa740
Frank Yournet <60455790+yournetf@users.noreply.github.com>
2024-07-15 20:58:02 -0700

    Updated university page to have media player

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,10 +1,18 @@
 import PropTypes from "prop-types";
-import "../Styles/Showcase.css"
+import "../Styles/Showcase.css";
+import { BiSolidLeftArrow } from "react-icons/bi";
+import { BiSolidRightArrow } from "react-icons/bi";
+
+
 function Showcase(props){
     return(
         <>
             <div className="showcaseDiv">
-                <p>{props.video1}</p>
+                <button id="leftArrow" className="arrowButton"><BiSolidLeftArrow /></button>
+                <div className="videoDiv">
+                    <video src="/assets/battleship.mp4"></video>
+                </div>
+                <button id="rightArrow" className="arrowButton"><BiSolidRightArrow /></button>
             </div>
         </>
     )
